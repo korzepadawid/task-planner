56798e4dd96e1e94cf63665991f007130ef36dcc
korzepadawid <korzepadawid@yahoo.com>
2021-11-11 20:16:43 +0100

    first commit

---
+++ src/main/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplication.java
@@ -0,0 +1,12 @@
+package io.github.korzepadawid.springtaskplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTaskPlanningApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTaskPlanningApplication.class, args);
+  }
+}
---
+++ src/test/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.korzepadawid.springtaskplanning;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class SpringTaskPlanningApplicationTests {
+
+  @Test
+  void shouldPass() {
+    assertTrue(true);
+  }
+}
