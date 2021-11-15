ca116a0984b5ce5bce787da9a9f26507b0ce49cc
korzepadawid <korzepadawid@yahoo.com>
2021-11-15 21:00:18 +0100

    feat: flyway migrations

---
+++ src/main/resources/db/migration/V1__users_and_avatars_tables_init.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS avatars;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+    id            BIGSERIAL PRIMARY KEY NOT NULL,
+    name          VARCHAR(255)          NOT NULL,
+    auth_provider VARCHAR(255)          NOT NULL,
+    password      VARCHAR(100)          NOT NULL,
+    email         VARCHAR(320) UNIQUE   NOT NULL,
+    created_at    TIMESTAMP DEFAULT NOW(),
+    updated_at    TIMESTAMP
+);
+
+CREATE TABLE avatars
+(
+    id         BIGSERIAL PRIMARY KEY NOT NULL,
+    uuid       VARCHAR(100) UNIQUE   NOT NULL,
+    photo      BYTEA,
+    user_id    BIGINT UNIQUE         NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP,
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) REFERENCES users (id)
+);
