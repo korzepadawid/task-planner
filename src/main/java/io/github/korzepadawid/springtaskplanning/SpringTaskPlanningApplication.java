2435cc7ddf853893a8cf88e40c980184af958e90
korzepadawid <korzepadawid@yahoo.com>
2021-12-23 20:18:40 +0100

    build: docker support

--- src/main/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplication.java
+++ src/main/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplication.java
@@ -2,11 +2,18 @@ package io.github.korzepadawid.springtaskplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class SpringTaskPlanningApplication {
+public class SpringTaskPlanningApplication extends SpringBootServletInitializer {
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(SpringTaskPlanningApplication.class);
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(SpringTaskPlanningApplication.class, args);
