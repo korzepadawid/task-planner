1a2ba41ea627df453dfa6a0271d3f9612924eb20
korzepadawid <korzepadawid@yahoo.com>
2021-12-28 17:07:18 +0100

    feat: routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Register from './pages/Register';
 
 const App: React.FC = () => (
-  <>
-    <h1>Hello frontend</h1>
-  </>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Register: React.FC = () => (
+  <>
+    <h1>Register</h1>
+  </>
+);
+
+export default Register;
