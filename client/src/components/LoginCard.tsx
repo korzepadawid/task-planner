d7cbf2ec4e7dc2253e4c4e5ed1b778b207170c55
korzepadawid <korzepadawid@yahoo.com>
2021-12-29 22:47:09 +0100

    feat: login card update

--- client/src/components/LoginCard.tsx
+++ client/src/components/LoginCard.tsx
@@ -1,53 +1,102 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@mui/material/Typography';
-import { Button, Card, CardContent, Grid, TextField } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import {
+  Alert,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+} from '@mui/material';
 
-const LoginCard: React.FC = () => (
-  <Card>
-    <CardContent>
-      <Grid container spacing={3} direction="column" alignItems="center">
-        <Grid item xs={12}>
-          <Typography variant="h6" component="h3" gutterBottom>
-            Login
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-basic"
-            label="Email"
-            variant="outlined"
-            size="small"
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            fullWidth
-            id="outlined-basic"
-            label="Password"
-            type="password"
-            variant="outlined"
-            size="small"
-          />
-        </Grid>
-        <Grid
-          item
-          container
-          spacing={3}
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item>
-            <Button variant="contained">Login</Button>
+const LoginCard: React.FC = () => {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit: VoidFunction = async () => {
+    setLoading(true);
+    const data = { email: 'string@email.com', password: 'string' };
+    const response = await fetch('http://localhost:8080/api/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.status === 400 || response.status === 403) {
+      setError(true);
+    } else {
+      setError(false);
+      const { accessToken } = await response.json();
+      console.log(accessToken);
+    }
+
+    setLoading(false);
+  };
+
+  return (
+    <Card>
+      <CardContent>
+        <Grid container spacing={3} direction="column" alignItems="center">
+          <Grid item xs={12}>
+            <Typography variant="h6" component="h3" gutterBottom>
+              Login
+            </Typography>
           </Grid>
-          <Grid item>
-            <Button href="#text-buttons">Register</Button>
+          {error && (
+            <Grid item xs={12}>
+              <Alert severity="error">
+                Ooops, invalid credentials. Try again, please!
+              </Alert>
+            </Grid>
+          )}
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              size="small"
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              type="password"
+              variant="outlined"
+              size="small"
+            />
+          </Grid>
+          <Grid
+            item
+            container
+            spacing={3}
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <LoadingButton
+                variant="contained"
+                onClick={handleSubmit}
+                loading={loading}
+              >
+                Login
+              </LoadingButton>
+            </Grid>
+            <Grid item>
+              <Button href="#text-buttons">Register</Button>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-    </CardContent>
-  </Card>
-);
+      </CardContent>
+    </Card>
+  );
+};
 
 export default LoginCard;
