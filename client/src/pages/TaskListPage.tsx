1cfd96f8594ff131ed768b1617e6c9756d1cadb1
korzepadawid <korzepadawid@yahoo.com>
2022-01-22 21:24:01 +0100

    feat: stats

--- client/src/pages/TaskListPage.tsx
+++ client/src/pages/TaskListPage.tsx
@@ -1,24 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { CircularProgress, Typography } from '@mui/material';
 import { MainState } from '../store';
 import { GET_TASK_LIST_URL } from '../constants/urls';
+import { fromUnixToTodayDistance } from '../util/time';
 
 interface Params {
   id: string;
 }
 
+interface TaskList {
+  createdAt: number;
+  done: number;
+  id: number;
+  title: string;
+  total: number;
+  undone: number;
+}
+
 const TaskListPage: React.FC = () => {
-  const [taskList, setTaskList] = useState();
+  const [taskList, setTaskList] = useState<TaskList>({
+    createdAt: 1642882196.760495,
+    done: 0,
+    id: 5,
+    title: 'string 4',
+    total: 0,
+    undone: 0,
+  });
+  const [loading, setLoading] = useState(false);
   const [tasks, setTasks] = useState([]);
-  const { search } = useLocation();
-  const page = new URLSearchParams(search).get('page');
   const { id } = useParams<Params>();
   const { accessToken } = useSelector((state: MainState) => state);
+
   useEffect(() => {
     const fetchTaskListDetails = async (): Promise<void> => {
       if (id.length > 0) {
+        setLoading(true);
         try {
           const { data: listDetails } = await axios.get(
             `${GET_TASK_LIST_URL}${id}`,
@@ -32,7 +51,7 @@ const TaskListPage: React.FC = () => {
           console.log(listDetails);
 
           const { data: tasksForCurrentTaskList } = await axios.get(
-            `http://localhost:8080/api/v1/task-lists/${id}/tasks?page=${page}`,
+            `http://localhost:8080/api/v1/task-lists/${id}/tasks`,
             {
               headers: {
                 Authorization: `Bearer ${accessToken}`,
@@ -41,8 +60,10 @@ const TaskListPage: React.FC = () => {
           );
           console.log(tasksForCurrentTaskList);
           setTasks(tasksForCurrentTaskList);
+          setLoading(false);
         } catch {
           console.error('an error occurred');
+          setLoading(false);
         }
       }
     };
@@ -50,9 +71,23 @@ const TaskListPage: React.FC = () => {
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
+  if (loading) {
+    return <CircularProgress />;
+  }
+
   return (
     <>
-      <h1>Task list page {parseInt(id, 10)}</h1>
+      <Typography variant="h4" component="h1" gutterBottom>
+        {taskList?.title}
+      </Typography>
+      <Typography variant="h5" component="h2" gutterBottom>
+        Total: {taskList?.total} ,Done: {taskList?.done}, Undone:
+        {taskList?.undone}
+      </Typography>
+      <Typography variant="h6" component="h4" gutterBottom>
+        {fromUnixToTodayDistance(taskList.createdAt)}
+      </Typography>
     </>
   );
 };
