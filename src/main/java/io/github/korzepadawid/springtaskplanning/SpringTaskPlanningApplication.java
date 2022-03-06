2d1c07e07014d16856b9c4fda1d4245946c40ee0
korzepadawid <korzepadawid@yahoo.com>
2022-03-06 20:08:57 +0100

    fix: removed servlet initializer

--- src/main/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplication.java
+++ src/main/java/io/github/korzepadawid/springtaskplanning/SpringTaskPlanningApplication.java
@@ -2,18 +2,11 @@ package io.github.korzepadawid.springtaskplanning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-public class SpringTaskPlanningApplication extends SpringBootServletInitializer {
-
-  @Override
-  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    return application.sources(SpringTaskPlanningApplication.class);
-  }
+public class SpringTaskPlanningApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SpringTaskPlanningApplication.class, args);
